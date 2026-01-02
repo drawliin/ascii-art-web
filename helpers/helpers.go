ac08d25fbac65fbdcc8d50c2b237ad709a57fd8f
hhamouich <boomepic99@gmail.com>
2026-01-02 13:21:26 +0100

    move structs and helping funcs to helpers file

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,5 +1,23 @@
 package helpers
 
+import (
+	"bytes"
+	"net/http"
+	"text/template"
+)
+
+type PageData struct {
+	UserInput string
+	Font      string
+	Art       string
+	ErrorMsg  string
+}
+
+type AnError struct {
+	Code    int
+	Message string
+}
+
 // Splits banner file's content into a slice of ascii representations
 // Returns a slice of ascii represented symbols (each of them as a slice of 8 lines/strings)
 func Split2D(s string) [][]string {
@@ -35,4 +53,36 @@ func ContainOnlyNewLines(arr []string) bool {
 		}
 	}
 	return true
+}
+
+func ErrorPage(w http.ResponseWriter, status int) {
+	tmpl, err := template.ParseFiles("templates/error.html")
+	if err != nil {
+		// error in the error page *o*
+		w.WriteHeader(500)
+		w.Write([]byte("500 Internal Server Error"))
+		return
+	}
+
+	w.WriteHeader(status)
+
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, AnError{
+		Code:    status,
+		Message: http.StatusText(status),
+	})
+	if err != nil {
+		ErrorPage(w, http.StatusInternalServerError)
+		return
+	}
+	buf.WriteTo(w)
+}
+
+func ValidFont(s string) bool {
+	switch s {
+	case "standard", "shadow", "thinkertoy":
+		return true
+	default:
+		return false
+	}
 }
